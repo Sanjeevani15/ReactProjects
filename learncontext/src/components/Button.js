f0fb26d3271c64c8437beb4c3bf1511c62933775
Sanjeevani Bhandari <sanjeevanibhandari@Sanjeevani-Bhandari-I156s-MacBook-Pro.local>
2022-04-15 10:20:19 +0530

    added new folder

--- learncontext/src/components/Button.js
+++ learncontext/src/components/Button.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import LanguageContext from '../contexts/LanguageContext';
-import ColorContext from '../contexts/ColorContext';
+import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 const Button = () => {
   return (
     <>
-    <ColorContext.Consumer>
-    {(color)=>
-    <button className={`ui button ${color}`}>
-    <LanguageContext.Consumer>
-      {(value)=> value==='english'? 'Submit':'Jama Kre'}
-    </LanguageContext.Consumer>
-  </button>
-    }
-    </ColorContext.Consumer>
+      <ColorContext.Consumer>
+        {(color) => (
+          <button className={`ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {(value) => (value === "english" ? "Submit" : "Jama Kre")}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
     </>
-  )
-}
+  );
+};
 
 export default Button;
